d6439ee9f9764f18dbe36f2ec74e84fac2719359
Barbara Xavier <barbara.xavier@venturus.org.br>
2026-03-20 21:20:33 -0400

    feat: creating new video speed options

--- src/app/shared/modules/material.module.ts
+++ src/app/shared/modules/material.module.ts
@@ -3,8 +3,15 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { MatGridListModule } from '@angular/material/grid-list';
+import { MatMenuModule } from '@angular/material/menu';
 
 @NgModule({
-  exports: [MatCardModule, MatButtonModule, MatIconModule, MatGridListModule],
+  exports: [
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule,
+    MatGridListModule,
+    MatMenuModule
+  ],
 })
 export class MaterialModule {}
